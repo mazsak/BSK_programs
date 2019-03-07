198d5e50433c06cd500457ee3f0665ec0da82864
Andzik <smers20@wp.pl>
2019-03-07 23:07:20 +0100

    CAEsar prawie

--- BSKgrupa/src/main/java/bskgrupa/CaesarCipher.java
+++ BSKgrupa/src/main/java/bskgrupa/CaesarCipher.java
@@ -1,7 +1,9 @@
 package bskgrupa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CaesarCipher {
 
@@ -46,6 +48,27 @@ public class CaesarCipher {
     public static String encryptB(String word, int keyA, int keyB) {
         StringBuilder reply = new StringBuilder();
         List<Character> tab = new ArrayList<>();
+        word = word.replaceAll("\\s", "");
+
+        for (int i = 0; i < word.length(); i++) {
+            tab.add(word.charAt(i));
+        }
+
+        for (char letter : tab) {
+            if (letter >= 65 && letter <= 90) {
+                System.out.println( ((((letter - 65) * keyA + keyB) % 26) + 65));
+                reply.append((char) ((((letter - 65) * keyA + keyB) % 26) + 65));
+            }else {
+                reply.append((char) ((((letter - 97) * keyA + keyB) % 26) + 97));
+            }
+        }
+
+        return reply.toString();
+    }
+
+    public static String decryptB(String word, int keyA, int keyB) {
+        StringBuilder reply = new StringBuilder();
+        List<Character> tab = new ArrayList<>();
 
         word = word.replaceAll("\\s", "");
 
@@ -54,10 +77,11 @@ public class CaesarCipher {
         }
 
         for (char letter : tab) {
-            if (letter >= 65 && letter <= 90)
-                reply.append((char) ((((letter - 65) * keyA + keyB) % 26) + 65));
-            else
-                reply.append((char) ((((letter - 97) * keyA + keyB) % 26) + 97));
+            if (letter >= 65 && letter <= 90) {
+                reply.append((char) (((((letter - 65) + (26 - keyA)) * Math.pow(keyB, 11)) % 26) + 65));
+            }else{
+                reply.append((char) (((((letter - 97) + (26 - keyA)) * Math.pow(keyB, 20)) % 26) + 97));
+            }
         }
 
         return reply.toString();
