33d0816b00ad4ab388b95dad2e4578e229067826
Andzik <smers20@wp.pl>
2019-03-19 18:15:02 +0100

    xor xD

---
+++ BSK45/src/main/java/sample/Zad1.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Zad1 {
+
+    public static int lsfr(int wielomian, int seed){
+
+
+        return 1;
+    }
+
+    public int xor(int first, int second){
+        return (first+second)%2;
+    }
+
+    public int xor(int first, int second, int third){
+        return (first+second+third)%2;
+    }
+
+    public int xor(int first, int second, int third, int fourth){
+        return (first+second+third+fourth)%2;
+    }
+}
