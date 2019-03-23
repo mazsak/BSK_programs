888fd8f67eb09c180d68023f0500a777bc1e02e8
Mateusz Samluk <mazsak97@gmail.com>
2019-03-23 15:44:54 +0100

    CA decipher is finished to check in other examples

--- BSK45/src/main/java/sample/CA.java
+++ BSK45/src/main/java/sample/CA.java
@@ -1,5 +1,8 @@
 package sample;
 
+import com.google.inject.internal.util.Lists;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,7 +11,29 @@ public class CA {
     public List<Integer> encrypt(List<Integer> numberOfPower, List<Integer> input, List<Integer> X) {
         List<Integer> reply;
 
-        reply = AuxiliaryMethods.systemLDFR(numberOfPower, input, X).stream().map(e -> e.get(0)).collect(Collectors.toList()).subList(0, X.size());
+        reply = AuxiliaryMethods
+                .systemLDFR(numberOfPower, input, X)
+                .stream()
+                .map(e -> e.get(0))
+                .collect(Collectors.toList())
+                .subList(0, X.size());
+
+        return reply;
+    }
+
+    public List<Integer> decipher(List<Integer> numberOfPower, List<Integer> input, List<Integer> X) {
+        List<Integer> reply = new ArrayList<>();
+
+        int maxPower = numberOfPower.stream().collect(Collectors.summarizingInt(Integer::intValue)).getMax();
+        SystemD system = new SystemD(maxPower, input);
+        List<Integer> output;
+
+        int idx = 0;
+        do {
+            reply.add(XOR.calculate(Lists.newArrayList(system.getOutputByIndex(0), system.getOutputByIndex(maxPower - 1), X.get(idx))));
+            system.next(X.get(idx));
+            idx++;
+        } while (idx != X.size());
 
         return reply;
     }
