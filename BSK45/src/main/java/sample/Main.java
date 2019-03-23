57049b3477f3b47fde4b63a780ca375856072c31
Mateusz Samluk <mazsak97@gmail.com>
2019-03-23 15:53:38 +0100

    delete interface FXML

--- BSK45/src/main/java/sample/Main.java
+++ BSK45/src/main/java/sample/Main.java
@@ -1,33 +1,19 @@
 package sample;
 
 import com.google.inject.internal.util.Lists;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        LFSR cos = new LFSR();
-        cos.calculate(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0));
-        SSC cos1 = new SSC();
-        cos1.encryptAndDecipher(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(1, 0, 0, 0));
-        cos1.encryptAndDecipher(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(0, 1, 0, 1));
-        CA cos2 = new CA();
-        cos2.encrypt(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(1, 0, 0, 0));
-        cos2.decipher(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(0, 0, 1, 1));
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class Main {
 
 
     public static void main(String[] args) {
-        launch(args);
+        LFSR cos = new LFSR();
+        System.out.println(cos.calculate(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0)));
+        SSC cos1 = new SSC();
+        System.out.println(cos1.encryptAndDecipher(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(1, 0, 0, 0)));
+        System.out.println(cos1.encryptAndDecipher(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(0, 1, 0, 1)));
+        CA cos2 = new CA();
+        System.out.println(cos2.encrypt(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(1, 0, 0, 0)));
+        System.out.println(cos2.decipher(Polynomial.read("x^3+x+1"), Lists.newArrayList(1, 0, 0), Lists.newArrayList(0, 0, 1, 1)));
+
     }
 }
