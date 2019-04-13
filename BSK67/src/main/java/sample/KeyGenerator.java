f01e205bedf412fa1ed88046d42501866c7d6be8
dan47 <00kupa0@gmail.com>
2019-04-13 19:24:27 +0200

    Generowanie kluczy (za 2pkt)

---
+++ BSK67/src/main/java/sample/KeyGenerator.java
@@ -0,0 +1,64 @@
+package sample;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class KeyGenerator {
+    private static String key;
+    private static String[] keysCombined;
+
+    public static void loadKey() throws FileNotFoundException {
+        StringBuilder sb = new StringBuilder();
+        try (Reader reader = Files.newBufferedReader(Paths.get("key.txt"))) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                char ch = (char) c;
+                int value = Character.getNumericValue(ch);
+                if (value >= 0) {
+                    sb.append(value);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        key = sb.toString();
+    }
+
+    public static void permutationPC1(){
+        StringBuilder sb = new StringBuilder();
+        int pc1[] = Tables.getPc1();
+        for(int i = 0; i<pc1.length; i++){
+            sb.append(key.charAt(pc1[i]-1));
+        }
+        key = sb.toString();
+    }
+
+    public static String permutationPC2(String keyPC){
+        StringBuilder sb = new StringBuilder();
+        int pc2[] = Tables.getPc2();
+        for(int i = 0; i<pc2.length; i++){
+            sb.append(keyPC.charAt(pc2[i]-1));
+        }
+        return sb.toString();
+    }
+
+    public static void divide(){
+        String leftKey = key.substring(0,28);
+        String rightKey = key.substring(28,56);String keyCombined;
+        int[] leftShiftOrder = Tables.getLeftShiftOrder();
+        for(int i = 0; i<16; i++){
+            leftKey = leftKey.substring(leftShiftOrder[i],28) + leftKey.substring(0,leftShiftOrder[i]);
+            rightKey = rightKey.substring(leftShiftOrder[i],28) + rightKey.substring(0,leftShiftOrder[i]);
+            keyCombined = leftKey + rightKey;
+            keyCombined = permutationPC2(keyCombined);
+            System.out.println(keyCombined);
+        }
+    }
+
+    public static String getKey() {
+        return key;
+    }
+}
--- BSK67/src/main/java/sample/Main.java
+++ BSK67/src/main/java/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+
 public class Main extends Application {
 
     @Override
@@ -18,6 +20,14 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        try {
+            KeyGenerator.loadKey();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        KeyGenerator.permutationPC1();
+        KeyGenerator.divide();
+
         launch(args);
     }
 }
